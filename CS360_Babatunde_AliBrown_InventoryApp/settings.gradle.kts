d72076621ee360a3e389b2cfe3127766414a31cc
Ali-Brown <111250111+Ali-Brown@users.noreply.github.com>
2024-09-26 20:33:10 -0400

    CS 360 Project files upload

---
+++ CS360_Babatunde_AliBrown_InventoryApp/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CS360_Babatunde_AliBrown_InventoryApp"
+include(":app")
